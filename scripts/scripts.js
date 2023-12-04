750deba5735a9bdb7673df0d679a7d4e860addfb
Mathew Ross <matjross@gmail.com>
2023-12-04 09:33:09 -0700

    adding first portions of code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Mathew Ross
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -10,10 +10,8 @@ You are encouraged to use the provided naming convention for ease of review.
 /* create variables to hold the values for modelName and duration */
 
 // INSERT YOUR CODE HERE
-
-
-
-
+const modelName = document.getElementById('model-button');
+const duration = document.getElementById('duration-button');
 
 /****************** helper function ******************/
 /* create a function called recalculate() which will
@@ -26,11 +24,16 @@ You are encouraged to use the provided naming convention for ease of review.
 */
 
 // INSERT YOUR CODE HERE
-
-
-
-
-
+function recalculate() {
+    let costLabel= document.getElementById('calculated cost');
+    if (modelName === 'XYZ') {
+        let totalCost = duration*100
+    }
+    else if (modelName === 'CPRG') {
+        let totalCost = duration*213
+    }
+    costLabel = totalCost
+}
 
 /****************** model button logic ******************/
 
